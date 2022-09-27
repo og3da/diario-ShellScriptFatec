03299ae24954d387dcc3d2e9bf625187287bbb90
og3da <rapha488@gmail.com>
2022-09-26 22:30:16 -0300

    programa que exibe informações de Rede melhorado

---
+++ comandosIP_v2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# se digitar diferente de -h ou +1 paramtro, retorna msg de erro
+clear
+echo
+
+if [ "$#" -gt 1 ]
+then
+ echo 'erro'
+
+elif [ "$1" == "-h" ]
+then
+ echo 'Este é um programa que mostra info de redes'
+
+elif [ "$#" -eq 0 ]
+    then
+    echo -n '
+    1- ver ip
+    2- tabela host
+    3- info de roteamento: '
+    read OPC
+
+    case "$OPC" in
+    1) ip a;;
+    2) cat /etc/hosts;;
+    3) ip route;;
+    *) echo 'opcao invalida';;
+    esac
+
+else
+ echo 'erro'
+fi
