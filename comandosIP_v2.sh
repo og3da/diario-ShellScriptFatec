76edc2d2ad216cb066d02a03bd8c735da8ea0f06
og3da <rapha488@gmail.com>
2022-10-05 20:12:07 -0300

    atualizando opções; p1

--- comandosIP_v2.sh
+++ comandosIP_v2.sh
@@ -9,20 +9,22 @@ then
 
 elif [ "$1" == "-h" ]
 then
- echo 'Este é um programa que mostra info de redes'
+ echo 'Este é um programa que exibe informações de redes'
 
 elif [ "$#" -eq 0 ]
     then
     echo -n '
     1- ver ip
     2- tabela host
-    3- info de roteamento: '
+    3- info de roteamento 
+    4- info de slots pci: '
     read OPC
 
     case "$OPC" in
     1) ip a;;
     2) cat /etc/hosts;;
     3) ip route;;
+    4) lspci;;
     *) echo 'opcao invalida';;
     esac
 
